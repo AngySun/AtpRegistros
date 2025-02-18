00c23da1e6dd93895ac42225d2cd99af80af5694
Angel Arroyo <angel.torres.08@hotmail.com>
2025-02-17 22:03:34 -0600

    Agregar archivos de proyecto.

---
+++ AtpRegistros/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AtpRegistros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogDataGrid = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.saveFileDialogDataGrid = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialogDataGrid
+            // 
+            this.openFileDialogDataGrid.FileName = "openFileDialogDataGrid";
+            this.openFileDialogDataGrid.Filter = "Archivo CSV|*.csv";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(3, 31);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowHeadersWidth = 51;
+            this.dgvDatos.RowTemplate.Height = 24;
+            this.dgvDatos.Size = new System.Drawing.Size(794, 407);
+            this.dgvDatos.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Abrir Registros";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogDataGrid;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogDataGrid;
+    }
+}
+
---
+++ AtpRegistros/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtpRegistros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialogDataGrid.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    string filename = openFileDialogDataGrid.FileName;
+                    System.IO.StreamReader sr = new System.IO.StreamReader(filename);
+                    string linea;
+                    linea = sr.ReadLine();
+                    //Guarda la primera linea de encabezado
+                    string[] encabezado = linea.Split(',');
+                    //Agrega el numero de columnas 
+                    dgvDatos.ColumnCount = encabezado.Length;
+                    //Agregar el encabezado a dgvDatos.
+                    dgvDatos.Rows.Add(encabezado);
+                    
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        string[] campos = linea.Split(',');
+                        dgvDatos.Rows.Add(campos);
+                    }
+                }catch(System.IO.IOException si)
+                {
+                    MessageBox.Show("El programa esta abierto en otra parte. Cierralo e intentalo de nuevo");
+                }
+            }    
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void butGuardar_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = saveFileDialogDataGrid.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string filename = saveFileDialogDataGrid.FileName;
+                int total = dgvDatos.Rows.Count - 1;
+                string[] lineas = new string[total];
+                for (int i = 0; i < total; i++)
+                {
+                    string linea = "";
+                    for (int j = 0; j < dgvDatos.Columns.Count; j++)
+                    {
+                        if (j == dgvDatos.Columns.Count - 1)
+                            linea += dgvDatos.Rows[i].Cells[j].Value.ToString() + "";
+                        else
+                            linea += dgvDatos.Rows[i].Cells[j].Value.ToString() + ",";
+                    }
+                    lineas[i] = linea;
+                }
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
